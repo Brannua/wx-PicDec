0c204d0f60c1a6ee2c99e5334ec0546e34837a23
Brannua <374453156@qq.com>
2019-01-12 22:23:33 +0800

    canvas将相册里的图片画在了第二个页面上

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,6 @@
+var app = getApp();// 获取app实例
+
+
 Page({
   uploadPhoto: function() {
     wx.chooseImage({
@@ -5,8 +8,13 @@ Page({
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
       success: function(res) {
+
+        app.globalData.uploadData = res;
+        wx.navigateTo({
+          url: '/pages/scene/scene',
+        })
         // tempFilePath可以作为img标签的src属性显示图片
-        const tempFilePaths = res.tempFilePaths
+        // const tempFilePaths = res.tempFilePaths
       }
     })
   }
